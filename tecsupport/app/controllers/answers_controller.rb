bdff230c2e8d0330a85c167649a15e7eda051754
Laura <laura.s.chavez@outlook.com>
2018-09-13 14:46:37 -0400

    view is with error answer

--- tecsupport/app/controllers/answers_controller.rb
+++ tecsupport/app/controllers/answers_controller.rb
@@ -1,22 +1,19 @@
 class AnswersController < ApplicationController
-    before_action :set_answer, only: [:show, :edit, :update, :destroy]
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
+  before_action :set_answer, only: [:show, :edit, :update, :destroy]
+  
+  
     
-
+    
+   
     def new 
       redirect_to questions_path, notice: 'You must be logged in to answer' if !(current_user)
       @answer = Answer.new 
     end 
     
     def create 
-      @answer = Answer.new(answer_params) 
-      @answer.valid = true
-      
-      if 
-        @answer.save
-        redirect_to questions_path, notice: 'answer was successfully launched.'
-      else
-        redirect_to  questions_path, notice: 'answer not successfully launched.'
-      end
+        @question = Answer.find(params[:question_id])
+        @answer = @question.answer.create(answer_params)
     end
   
     def edit 
@@ -47,7 +44,7 @@ class AnswersController < ApplicationController
   end
   
   def answer_params
-    params.require(:answer).permit(:user_id, :body, :valid, :question_id)
+    params.require(:answer).permit(:user_id, :body, :question_id)
   end
   
   end
